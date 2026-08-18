58f9a877fce1dfa5ade1fbd70d17e9f23530da25
Fran Silva <franaliado@gmail.com>
2026-08-18 19:00:09 -0400

    Envio de correo con Mailtrap 9

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -2,18 +2,17 @@ import nodemailer from 'nodemailer';
 
 export async function sendEmail({ to, subject, html }: { to: string, subject: string, html: string }) {
   const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST || "live.smtp.mailtrap.io",
-    port: Number(process.env.SMTP_PORT) || 2525,
+    host: process.env.SMTP_HOST || "send.smtp.mailtrap.io",
+    port: Number(process.env.SMTP_PORT) || 587,
     auth: {
-      user: process.env.SMTP_USER || "api",
-      pass: process.env.SMTP_PASS || "0f515938e42f323e92f900fb01f188ac"
+      user: process.env.SMTP_USER || "",
+      pass: process.env.SMTP_PASS || ""
     }
   });
 
   try {
     const info = await transporter.sendMail({
-      // Usamos el dominio genérico permitido por Mailtrap para evitar el bloqueo de dominio
-      from: '"Ferreira Academy" <no-reply@demomailtrap.co>',
+      from: '"Ferreira Academy" <no-reply@mailtrap.email>',
       to,
       subject,
       html,
